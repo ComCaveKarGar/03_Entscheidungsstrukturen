fa235a9426fcebf6a6904463142a9e77ec54183d
Karsten <Moonjumper@gmx.de>
2023-05-10 10:20:34 +0200

    Vorbereitung Wahrheitswerte

--- script_01.js
+++ script_01.js
@@ -3,3 +3,40 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+
+const ageJohn = 29;
+const ageMark = 30;
+
+// Deklaration
+
+let isJohnOlder, isJohnEqual, isJohnYounger;
+
+// Test | Logische Aussage
+isJohnYounger = (ageJohn < ageMark)
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("----------------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)
+// if(false)
+
+// if(isJohnOlder)
+// {
+// console.log("John ist älter")
+// }
+
+// if(isJohnYounger)
+// {
+// console.log("John ist jünger")
+// }
